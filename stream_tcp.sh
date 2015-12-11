6b557f439a93c2d59e8b0e8d58258b318c9d7de8
ShubhamCpp <shubham.chopra2906@gmail.com>
2015-12-11 17:11:21 +0530

    Create stream_tcp.sh

---
+++ stream_tcp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+clear
+raspivid -t 0 -h 720 -w 1080 -fps 25 -hf -b 2000000 -o - | gst-launch-1.0 -v fdsrc ! h264parse !  rtph264pay config-interval=1 pt=96 ! gdppay ! tcpserversink host=RASPBERRYIP port=5000
